c441c26c4f1d870af3ce265525da0883b906487f
Alisha Fatima <alishafatima6768@gmail.com>
2026-07-21 19:19:14 +0500

    auth and order service implementation

---
+++ auth-service/src/app/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from '@nestjs/common';
+import { AppService } from './app.service';
+import {MessagePattern , Payload} from '@nestjs/microservices';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  @MessagePattern({
+    cmd: 'validate_user'
+  })
+  handleUserValidation(@Payload() data: any){
+    console.log('Auth service received data: ', data);
+    if(data.userId === 1){
+      return {status: 'success' , user:{
+        id: 1 ,
+        name: 'Alisha'
+      }};
+    }
+    return {status: 'error' , message: 'User not found'};
+  }
+
+}
